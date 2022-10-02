368d48868fcd30c6ea45fb541a780170df822122
Kanatbek-AKA <61031156+Kanatbek-AKA@users.noreply.github.com>
2022-10-02 15:43:26 +0100

    Create map.js

---
+++ static/js/map.js
@@ -0,0 +1,8 @@
+// Google map sample
+// function Map() {
+//   // location
+//   const locate = {lat: 52.370216, lng: 4.895168} ;
+//   const map = new google.maps.Map(document.getElementById("map"), {zoom: 4, center: locate});
+//   const marker = new google.maps.Marker({position: locate, map: map});
+// };
+// window.initMap = Map;
